657d1833c920539f9105d2c33107b89d3143087b
fox0 <red-fox0@yandex.ru>
2018-12-07 16:34:31 +0700

    попытка заанимировать

--- src/player.lua
+++ src/player.lua
@@ -1,19 +1,32 @@
 local SPEED = 400
-local IMG = love.graphics.newImage('img/love-ball.png')
+
+-- todo вынести анимацию в отдельный класс
+--https://github.com/fox0/LOVE-Example-Browser/blob/master/animation.lua
+
+local FRAMES = {}
+for i = 12, 17 do
+    -- todo bug
+    FRAMES[#FRAMES] = love.graphics.newImage('img/6_' .. i .. '.png')
+end
 
 local Player = {}
 Player.__index = Player
 
 Player.init = function(self, x, y)
     local obj = {}
-    setmetatable(obj, Player)
-    obj.image = IMG
     obj.x = x
     obj.y = y
-    return obj
+    obj._timer = 0
+    obj._index_frame = 1
+    return setmetatable(obj, Player)
 end
 
 Player.update = function(self, dt)
+    self:_update_coord(dt)
+    self:_update_animation(dt)
+end
+
+Player._update_coord = function(self, dt)
     if love.keyboard.isDown('a') or love.keyboard.isDown('left') then
         self.x = self.x - SPEED * dt
     end
@@ -28,8 +41,22 @@ Player.update = function(self, dt)
     end
 end
 
+Player._update_animation = function(self, dt)
+    self._timer = self._timer + dt * 400
+    log.debug(self._timer)
+    if self._timer > 40 then
+        self._timer = self._timer - 40
+        self._index_frame = self._index_frame + 1
+        if self._index_frame > #FRAMES then
+            self._index_frame = 0
+        end
+    end
+end
+
 Player.draw = function(self)
-    love.graphics.draw(self.image, self.x, self.y)
+    -- todo хранить ссылку на спрайт
+    log.debug(self._index_frame) -- todo bug
+    love.graphics.draw(FRAMES[self._index_frame], self.x, self.y)
 end
 
 return Player
