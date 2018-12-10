445ea01225d9893a153ba12265440b83d66cd54d
fox0 <red-fox0@yandex.ru>
2018-12-10 21:34:27 +0700

    fix бага отрисовки спрайта

--- src/frame.lua
+++ src/frame.lua
@@ -28,13 +28,33 @@ end
 ---@param y number
 ---@param r number
 function Frame.draw(self, x, y, r)
-    local sx = 1
-    local sy = 1
-    if self._is_xmirror then  --todo bug зеркалировать не при выводе, а сам спрайт
+    local sx, sy, ox, oy = 1, 1, 0, 0
+    if self._is_xmirror then
         sx = -1
-        x = x + self.H
+        ox = self.W
     end
-    love.graphics.draw(self._image, self._quad, x, y, r, sx, sy)
+    love.graphics.draw(self._image, self._quad, x, y, r, sx, sy, ox, oy)
+    self:_debug_draw(x, y, r)
+end
+
+function Frame._debug_draw()
+
+end
+
+if log.level == 'debug' then
+    ---@param self Frame
+    ---@param x number
+    ---@param y number
+    ---@param r number
+    function Frame._debug_draw(self, x, y, r)
+        local r_, g, b, a = love.graphics.getColor()
+        love.graphics.setColor(.0, 1., .0, 1.)
+        love.graphics.circle('line', x, y, 3)
+        local x1 = x + math.cos(r) * self.W
+        local y1 = y + math.sin(r) * self.H
+        love.graphics.line(x, y, x1, y1)
+        love.graphics.setColor(r_, g, b, a)
+    end
 end
 
 return Frame
