e8dafa747bb4628334dbf7dd96ab103677e38301
fox0 <red-fox0@yandex.ru>
2020-01-17 11:53:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ VERSION=`grep -o '[0-9]\{1,2\}\.[0-9]\{1,2\}\.[0-9]\{1,2\}' conf.lua`
 NAME="dist/NamelessHero${VERSION}.love"
 
 echo "Building ${NAME}"
+mkdir dist
 zip -u ${NAME} *.lua
 zip -u ${NAME} src/*.lua
 zip -u ${NAME} love2d_camera/*.lua
